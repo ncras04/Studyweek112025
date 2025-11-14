b2c4b763e0af850d07a42d2c336ad5bfe33e8bdc
FabianSmi <darkscorpion1@gmx.de>
2025-11-14 14:09:25 +0100

    Edited: Storage Stuff

--- Assets/Scripts/SouvenirManagement/SouvenirManager.cs
+++ Assets/Scripts/SouvenirManagement/SouvenirManager.cs
@@ -1,24 +1,25 @@
 using UnityEngine;
-using static UnityEngine.Rendering.DebugUI;
 
-public class StorageWorthValue : MonoBehaviour
+public class SouvenirManager : MonoBehaviour
 {
     public SouvenirSingle[] m_Souvenirs;
+    [SerializeField] FloatVariable m_value;
+    [SerializeField] IntVariable m_count;
     public float WorthValue;
 
     void Start()
     {
         m_Souvenirs = FindObjectsByType<SouvenirSingle>(
             FindObjectsInactive.Include, FindObjectsSortMode.None);
-        
+
 
         for (int i = 0; i < m_Souvenirs.Length; i++)
         {
-            WorthValue += m_Souvenirs[i].WorthValue;
+            WorthValue += m_Souvenirs[i].WorthValue * m_Souvenirs[i].Condition;
         }
+        m_value.Value = WorthValue;
+        m_count.Value = m_Souvenirs.Length;
     }
 
 
-
-    
 }
---
+++ Assets/Scripts/SouvenirManagement/SouvenirSingle.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class SouvenirSingle : MonoBehaviour
+{
+    [SerializeField] public float Condition = 1;
+    [SerializeField] public float WorthValue = 1;
+
+}
