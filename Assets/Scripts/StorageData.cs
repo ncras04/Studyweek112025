dbcf5f711b3cbdda1dcca5bc11e7ac083534ad50
Nicolas Schmitt <nschmitt@duck.com>
2025-11-14 13:10:07 +0100

    Added: StorageData

--- Assets/Scripts/StorageData.cs
+++ Assets/Scripts/StorageData.cs
@@ -3,5 +3,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "StorageData", menuName = "Scriptable Objects/StorageData")]
 public class StorageData : ScriptableObject
 {
-    
+    public FloatVariable StorageValue { get; private set; }
+
+    [SerializeField] private FloatVariable m_storageValue;
+    //[SerializeField] private PlayerData m_winningPlayer;
+
+    public void SetStorageValue(float _value)
+    {
+        StorageValue.Value = _value;
+    }
 }
